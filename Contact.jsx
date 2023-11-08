7c313de889fff4864f0443967b6668cb2e57eafc
jnett93 <49693463+jnett93@users.noreply.github.com>
2023-11-07 17:03:40 -0700

    Add files via upload
    added footer page contact page and about page

---
+++ Contact.jsx
@@ -0,0 +1,52 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import './Contact.css'
+
+export default function Contact() {
+
+  return(
+    <div className="app">
+      <div id="component">
+      <h1>Contact our team!</h1>
+      <div>
+        Monique Avila
+        <br></br>
+        <a href='emailto:monique.codes@icloud.com'><FontAwesomeIcon icon={faEnvelope} style={{color: "#a7c6fb",}} /> Email me</a>
+        <br></br>
+        <a href='https://github.com/Moniii333'><FontAwesomeIcon icon={faGithub} /> Find me on GitHub</a>
+        <br></br>
+        <a href='www.linkedin.com/in/moniqueavila92'><FontAwesomeIcon icon={faLinkedin} /> Follow me on LinkedIn</a>
+      </div>
+      <div>
+        Garren Pho
+        <br></br>
+        <a href='emailto:'><FontAwesomeIcon icon={faEnvelope} style={{color: "#a7c6fb",}} /> Email me</a>
+        <br></br>
+        <a href='https://github.com/'><FontAwesomeIcon icon={faGithub} /> Find me on GitHub</a>
+        <br></br>
+        <a href='www.linkedin.com/'><FontAwesomeIcon icon={faLinkedin} /> Follow me on LinkedIn</a>
+      </div>
+      <div>
+        Jonathan Netterstrom
+        <br></br>
+        <a href='emailto:'><FontAwesomeIcon icon={faEnvelope} style={{color: "#a7c6fb",}} /> Email me</a>
+        <li>Netterstrom2000@gmail.com</li>
+        <a href='https://github.com/'><FontAwesomeIcon icon={faGithub} /> Find me on GitHub</a>
+        <p>jnett93</p>
+        <a href='www.linkedin.com/'><FontAwesomeIcon icon={faLinkedin} /> Follow me on LinkedIn</a>
+        <p>https://www.linkedin.com/in/jonathan-netterstrom/</p>
+      </div>
+      <div>
+        Rachel Watkins
+        <br></br>
+        <a href='emailto:'><FontAwesomeIcon icon={faEnvelope} style={{color: "#a7c6fb",}} /> Email me</a>
+        <br></br>
+        <a href='https://github.com/'><FontAwesomeIcon icon={faGithub} /> Find me on GitHub</a>
+        <br></br>
+        <a href='www.linkedin.com/'><FontAwesomeIcon icon={faLinkedin} /> Follow me on LinkedIn</a>
+      </div>
+    </div>
+    </div>
+  )
+}
